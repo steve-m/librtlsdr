f5f543e7bcca1a1aabd7ca2d048be020c90426d0
texane <texane@gmail.com>
2015-01-11 13:31:16 -0600

    rtlsdr_rpc_set_agc_mode

--- src/rtl_rpcd.c
+++ src/rtl_rpcd.c
@@ -817,6 +817,24 @@ static int handle_query
       break ;
     }
 
+  case RTLSDR_RPC_OP_SET_AGC_MODE:
+    {
+      uint32_t did;
+      uint32_t on;
+
+      PRINTF("set_agc_mode()\n");
+
+      if (rtlsdr_rpc_msg_pop_uint32(q, &did)) goto on_error;
+      if (rtlsdr_rpc_msg_pop_uint32(q, &on)) goto on_error;
+
+      if ((rpcd->dev == NULL) || (rpcd->did != did)) goto on_error;
+
+      err = rtlsdr_set_agc_mode(rpcd->dev, (int)on);
+      if (err) goto on_error;
+
+      break ;
+    }
+
   case RTLSDR_RPC_OP_RESET_BUFFER:
     {
       uint32_t did;
