024f3c57a247950673ae939e9063a1a50626ae4b
rxseger <rseger@gmx.co.uk>
2016-07-02 17:30:59 -0700

    rtl_ir: initialize dongle

--- src/rtl_ir.c
+++ src/rtl_ir.c
@@ -1,7 +1,170 @@
+/*
+ * rtl-sdr, turns your Realtek RTL2832 based DVB dongle into a SDR receiver
+ * Copyright (C) 2012 by Steve Markgraf <steve@steve-m.de>
+ * Copyright (C) 2012 by Hoernchen <la@tfc-server.de>
+ * Copyright (C) 2012 by Kyle Keen <keenerd@gmail.com>
+ * Copyright (C) 2013 by Elias Oenal <EliasOenal@gmail.com>
+ * Copyright (C) 2016 by Robert X. Seger <rseger@gmx.co.uk>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
+
+#include <errno.h>
+#include <signal.h>
+#include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+#ifndef _WIN32
+#include <unistd.h>
+#else
+#include <windows.h>
+#include <fcntl.h>
+#include <io.h>
+#include "getopt/getopt.h"
+#define usleep(x) Sleep(x/1000)
+#ifdef _MSC_VER
+#define round(x) (x > 0.0 ? floor(x + 0.5): ceil(x - 0.5))
+#endif
+#define _USE_MATH_DEFINES
+#endif
+
+#include <math.h>
+#include <pthread.h>
+#include <libusb.h>
+
+#include "rtl-sdr.h"
+#include "convenience/convenience.h"
+
+static volatile int do_exit = 0;
+
+struct dongle_state
+{
+	int      exit_flag;
+	pthread_t thread;
+	rtlsdr_dev_t *dev;
+	int      dev_index;
+};
+
+void dongle_init(struct dongle_state *s)
+{
+    s->dev_index = 0;
+}
+
+struct dongle_state dongle;
+
+void usage(void)
+{
+	fprintf(stderr,
+		"rtl_ir\n\n"
+		"Use:\trtl_ir [-options]\n"
+		"\t[-d device_index (default: 0)]\n");
+	exit(1);
+}
+
+#ifdef _WIN32
+BOOL WINAPI
+sighandler(int signum)
+{
+	if (CTRL_C_EVENT == signum) {
+		fprintf(stderr, "Signal caught, exiting!\n");
+		do_exit = 1;
+		rtlsdr_cancel_async(dongle.dev);
+		return TRUE;
+	}
+	return FALSE;
+}
+#else
+static void sighandler(int signum)
+{
+	fprintf(stderr, "Signal caught, exiting!\n");
+	do_exit = 1;
+	rtlsdr_cancel_async(dongle.dev);
+}
+#endif
+
+static void *dongle_thread_fn(void *arg)
+{
+	struct dongle_state *s = arg;
+    printf("TODO\n");
+	//rtlsdr_read_async(s->dev, rtlsdr_callback, s, 0, s->buf_len);
+	return 0;
+}
+
 
 int main(int argc, char **argv) {
-    printf("Hello, world!\n");
+#ifndef _WIN32
+	struct sigaction sigact;
+#endif
+	int r, opt;
+	int dev_given = 0;
+	dongle_init(&dongle);
+
+	while ((opt = getopt(argc, argv, "d:h")) != -1) {
+		switch (opt) {
+		case 'd':
+			dongle.dev_index = verbose_device_search(optarg);
+			dev_given = 1;
+			break;
+		case 'h':
+		default:
+			usage();
+			break;
+		}
+	}
+
+	if (dongle.dev_index < 0) {
+		exit(1);
+	}
+
+	r = rtlsdr_open(&dongle.dev, (uint32_t)dongle.dev_index);
+	if (r < 0) {
+		fprintf(stderr, "Failed to open rtlsdr device #%d.\n", dongle.dev_index);
+		exit(1);
+	}
+#ifndef _WIN32
+	sigact.sa_handler = sighandler;
+	sigemptyset(&sigact.sa_mask);
+	sigact.sa_flags = 0;
+	sigaction(SIGINT, &sigact, NULL);
+	sigaction(SIGTERM, &sigact, NULL);
+	sigaction(SIGQUIT, &sigact, NULL);
+	sigaction(SIGPIPE, &sigact, NULL);
+#else
+	SetConsoleCtrlHandler( (PHANDLER_ROUTINE) sighandler, TRUE );
+#endif
+
+	verbose_reset_buffer(dongle.dev);
+
+	usleep(100000);
+	pthread_create(&dongle.thread, NULL, dongle_thread_fn, (void *)(&dongle));
+
+	while (!do_exit) {
+		usleep(100000);
+	}
+
+	if (do_exit) {
+		fprintf(stderr, "\nUser cancel, exiting...\n");}
+	else {
+		fprintf(stderr, "\nLibrary error %d, exiting...\n", r);}
+
+	rtlsdr_cancel_async(dongle.dev);
+	pthread_join(dongle.thread, NULL);
+
+	rtlsdr_close(dongle.dev);
+	return r >= 0 ? r : -r;
+
     return 0;
 }
